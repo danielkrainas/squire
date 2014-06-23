0de59a8d7be05d402464559c7be6895777708deb
dkrainas <dkrainas87@gmail.com>
2014-06-23 02:02:14 -0400

    added decoupled extensions for the authorization namespace.

---
+++ src/Squire.Security/Authorization/DecoupledAuthorizationExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Squire.Security.Authorization
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Squire.Decoupled.Queries;
+    using Squire.Security.Authorization.Queries;
+
+    public static class DecoupledAuthorizationExtensions
+    {
+        public static ICollection<IRole> GetRolesForPlayer(this IDispatchQuery dispatch, IPlayer player)
+        {
+            return dispatch.Execute(new GetRolesForPlayer(player));
+        }
+    }
+}
---
+++ src/Squire.Security/Authorization/Queries/GetRolesForPlayer.cs
@@ -0,0 +1,24 @@
+﻿namespace Squire.Security.Authorization.Queries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Squire.Decoupled.Queries;
+    using Squire.Validation;
+
+    public class GetRolesForPlayer : IQuery<ICollection<IRole>>
+    {
+        public GetRolesForPlayer(IPlayer player)
+        {
+            player.VerifyParam("player").IsNotNull();
+            this.Player = player;
+        }
+
+        public IPlayer Player
+        {
+            get;
+            private set;
+        }
+    }
+}
