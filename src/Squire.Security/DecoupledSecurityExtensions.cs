2058bfd3ff965ad034b791d7229cf87d692c2bcc
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-07-01 17:54:54 -0400

    updated decoupled extensions to include new security related queries.

--- src/Squire.Security/DecoupledSecurityExtensions.cs
+++ src/Squire.Security/DecoupledSecurityExtensions.cs
@@ -6,17 +6,38 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using Squire.Validation;
 
     public static class DecoupledSecurityExtensions
     {
         public static IPlayer GetPlayerById(this IDispatchQuery dispatch, string id)
         {
+            dispatch.VerifyParam("dispatch").IsNotNull();
             return dispatch.Execute(new GetPlayerById(id));
         }
 
         public static IPlayer GetPlayerByName(this IDispatchQuery dispatch, string name)
         {
+            dispatch.VerifyParam("dispatch").IsNotNull();
             return dispatch.Execute(new GetPlayerByName(name));
+        }
+
+        public static IPlayer GetPlayerByEmail(this IDispatchQuery dispatch, string email)
+        {
+            dispatch.VerifyParam("dispatch").IsNotNull();
+            return dispatch.Execute(new GetPlayerByEmail(email));
+        }
+
+        public static ICollection<IPlayer> GetAllPlayers(this IDispatchQuery dispatch)
+        {
+            dispatch.VerifyParam("dispatch").IsNotNull();
+            return dispatch.Execute(new GetAllPlayers());
+        }
+
+        public static ICollection<IPlayer> GetPlayers(this IDispatchQuery dispatch, int pageIndex, int pageSize, string emailFilter = null, string nameFilter = null)
+        {
+            dispatch.VerifyParam("dispatch").IsNotNull();
+            return dispatch.Execute(new GetPlayers().WithEmailLike(emailFilter).WithNameLike(nameFilter).Page(pageIndex, pageSize));
         }
     }
 }
