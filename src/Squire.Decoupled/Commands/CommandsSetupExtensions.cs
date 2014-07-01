fdd6ae78ad8ec4afb635554cd7777494689a8fd0
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-07-01 13:49:41 -0400

    added setup API extensions for commands and events.

---
+++ src/Squire.Decoupled/Commands/CommandsSetupExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Squire.Decoupled.Commands
+{
+    using Squire.Decoupled.Pipeline;
+    using Squire.Setup;
+    using Squire.Validation;
+    using System;
+    using System.Linq.Expressions;
+
+    public static class CommandsSetupExtensions
+    {
+        public static IAppSetup<T> Commands<T>(this IAppSetup<T> setup, Expression<Func<PipelineDispatcherBuilder, PipelineDispatcherBuilder>> configure, IUpstreamHandler errorHandler = null)
+        {
+            configure.VerifyParam("configure").IsNotNull();
+            errorHandler = errorHandler ?? new DevNullUpstreamHandler();
+            var builder = configure.Compile()(new PipelineDispatcherBuilder(errorHandler));
+            if (builder == null)
+            {
+                throw new InvalidOperationException("the result of the configuration cannot be null.");
+            }
+
+            CommandDispatcher.Assign(builder.Build());
+            return setup;
+        }
+    }
+}
---
+++ src/Squire.Decoupled/DomainEvents/EventsSetupExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Squire.Decoupled.DomainEvents
+{
+    using Squire.Decoupled.Pipeline;
+    using Squire.Setup;
+    using Squire.Validation;
+    using System;
+    using System.Linq.Expressions;
+
+    public static class EventsSetupExtensions
+    {
+        public static IAppSetup<T> Events<T>(this IAppSetup<T> setup, Expression<Func<EventPipelineBuilder, EventPipelineBuilder>> configure, IUpstreamHandler errorHandler = null)
+        {
+            configure.VerifyParam("configure").IsNotNull();
+            errorHandler = errorHandler ?? new DevNullUpstreamHandler();
+            var builder = configure.Compile()(new EventPipelineBuilder(errorHandler));
+            if (builder == null)
+            {
+                throw new InvalidOperationException("the result of the configuration cannot be null.");
+            }
+
+            DomainEvent.Assign(builder.Build());
+            return setup;
+        }
+    }
+}
