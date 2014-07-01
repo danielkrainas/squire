30835ac6b481d57f68d669a7e665d18501c4c8f0
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-07-01 17:28:53 -0400

    more additions to the setup API

---
+++ src/Squire.Essentials/Setup/AdvancedAppSetup.cs
@@ -0,0 +1,57 @@
+﻿namespace Squire.Setup
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Squire.Validation;
+
+    public class AdvancedAppSetup<TAdvanced> : IAdvancedAppSetup<TAdvanced>
+    {
+        private readonly IAppSetup setup;
+
+        private readonly TAdvanced advanced;
+
+        public AdvancedAppSetup(IAppSetup setup, TAdvanced advanced)
+        {
+            setup.VerifyParam("setup").IsNotNull();
+            this.advanced = advanced;
+            this.setup = setup;
+        }
+
+        public IAppSetup AndThen
+        {
+            get
+            {
+                return setup;
+            }
+        }
+
+        public TAdvanced Advanced
+        {
+            get
+            {
+                return this.advanced;
+            }
+        }
+    }
+
+    public class AdvancedAppSetup<TApp, TAdvanced> : AdvancedAppSetup<TAdvanced>, IAdvancedAppSetup<TApp, TAdvanced>
+    {
+        private readonly IAppSetup<TApp> setup;
+
+        public AdvancedAppSetup(IAppSetup<TApp> setup, TAdvanced advanced)
+            : base(setup, advanced)
+        {
+            this.setup = setup;
+        }
+
+        public new IAppSetup<TApp> AndThen
+        {
+            get
+            {
+                return this.setup;
+            }
+        }
+    }
+}
---
+++ src/Squire.Essentials/Setup/IAdvancedAppSetup.cs
@@ -0,0 +1,28 @@
+﻿namespace Squire.Setup
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public interface IAdvancedAppSetup<out TAdvanced>
+    {
+        IAppSetup AndThen
+        {
+            get;
+        }
+
+        TAdvanced Advanced
+        {
+            get;
+        }
+    }
+
+    public interface IAdvancedAppSetup<out TApp, out TAdvanced> : IAdvancedAppSetup<TAdvanced>
+    {
+        new IAppSetup<TApp> AndThen
+        {
+            get;
+        }
+    }
+}
