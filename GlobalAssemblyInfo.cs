c3f01e42223d01a758b80251d9e6e127ae023a05
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-07-07 18:52:28 -0400

    release v0.2.3

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.2.2")]
+[assembly: AssemblyVersion("0.2.3")]
